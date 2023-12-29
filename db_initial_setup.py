e2c36dfd8ab388687c517522a5d5d36ea334a3e6
bojjinki <155133023+bojjinki@users.noreply.github.com>
2023-12-29 14:48:13 +0800

    Add db_initial_setup.py
    This file setups the Awming database and tables required to run the POS machine.
    
    This initial setup assumes that the Awming database and tables to be added have not been created yet.

---
+++ db_initial_setup.py
@@ -0,0 +1,47 @@
+import mysql.connector
+import csv
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Kea062702!"
+)
+
+mycursor = mydb.cursor()
+mycursor.execute("CREATE DATABASE Awming")
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Kea062702!",
+    database = "Awming"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE Table Inventory (Barcode VARCHAR(255), Item_Name VARCHAR(255), Category VARCHAR(255), Price INT, Stocks INT)")
+mycursor.execute("CREATE Table Sales (Barcode VARCHAR(255), Item_Name VARCHAR(255), Sale_Count INT, Price INT, Total_Price INT, Date DATE, Time TIME)")
+
+val = []
+with open('inventory_consolidation.csv', mode = 'r') as file:
+    csvFile = csv.reader(file)
+    line_count = 0
+    for lines in csvFile:
+        if (line_count != 0):
+            lines = tuple(lines)
+            val.append(lines) 
+
+        line_count += 1
+
+    sql = "INSERT INTO Inventory (Barcode, Item_Name, Category, Price, Stocks) VALUES (%s, %s, %s, %s, %s)"
+    mycursor.executemany(sql, val)
+    mydb.commit()
+
+    mycursor.execute("SHOW TABLES")
+
+    print("Awming database and essential tables created.")
+
+
+        
+        
+
